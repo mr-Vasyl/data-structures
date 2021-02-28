6338042a57fcb94a87d421c8f18eddb70a9426ee
mr-Vasyl <gerchak9999@gmail.com>
2021-02-28 20:30:20 +0200

    array

--- linear_search.js
+++ linear_search.js
@@ -1,1 +1,11 @@
-let a = 'a';
+// array
+const array = [ 1, 4, 5, 78, 8, 10, 2, 34 ];
+let count = 0;
+function linearSearch(array, item) {
+	for (let i = 0; i < array.length; i++) {
+		count += 1;
+		if (array[i] === item) return i;
+	}
+	return null;
+}
+console.log('index = ', linearSearch(array, 10), 'count = ', count);
